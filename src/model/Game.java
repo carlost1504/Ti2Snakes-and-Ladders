a8c3c4667c38e4d25e838b72d8a92a67c6486bf4
juandiegonieto <61235591+juandiegonieto@users.noreply.github.com>
2021-10-27 09:18:13 -0500

    first commit

---
+++ src/model/Game.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Game {
+	
+	private cell_matrix first;
+	private cell_matrix last;
+	private long size;
+	
+	
+	
+}	
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,10 +1,35 @@
 package ui;
 
+import java.util.Scanner;
+
 public class Main {
-
+	
+	private Scanner in;
+	
+	
+	
 	public static void main(String...args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("**************************");
+		System.out.println("**Snake and  Ladders Inc**");
+		System.out.println("**************************");
+		
+		
 	}
-
+	
+	public Main() {
+		in = new Scanner(System.in);
+	}
+	
+	public int showMenu() {
+		System.out.println("Elegir una opcion: ");
+		System.out.println("1. Jugar\n 2.Puntajes \n 3. Salir");
+		int optionAsnwer = Integer.parseInt(in.nextLine());
+		return optionAsnwer;
+	}
+	
+	public void showGame() {
+		
+	}
+	
 }
